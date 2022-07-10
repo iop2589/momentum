078469a202d7352421e21f06ff7fc0d26b75bb18
iop2589 <qkrtjdcjf07@naver.com>
2022-07-10 20:42:32 +0900

    todo-list 기능 구현 완료

---
+++ js/todo.js
@@ -0,0 +1,59 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector("#todo-list");
+let toDos = [];
+const TODOS_KEY = "toDos";
+
+function saveTodos () {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function toDosFilter (toDosId, liId) {
+    return toDosId !== liId;
+}
+
+function deleteTodo (event) {
+    const li = event.target.parentElement;
+    toDos = toDos.filter((item) => toDosFilter(item.id, parseInt(li.id)));
+    li.remove();
+    saveTodos();
+}
+
+function paintTodo (newTodoObj) {
+    const newTodoLi = document.createElement("li");
+    const newTodoSpan = document.createElement("span");
+    const toDoDeleteBtn = document.createElement("button");
+
+    toDoDeleteBtn.innerText = "❌";
+    toDoDeleteBtn.addEventListener("click", deleteTodo);
+    newTodoSpan.innerText = newTodoObj.text;
+    newTodoLi.id = newTodoObj.id;
+    newTodoLi.appendChild(newTodoSpan);
+    newTodoLi.appendChild(toDoDeleteBtn);
+    toDoList.appendChild(newTodoLi);
+}
+
+function handleTodoSubmit (event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now()
+    };
+
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
+    saveTodos();
+}
+
+toDoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !==  null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach((item) => paintTodo(item));
+}
